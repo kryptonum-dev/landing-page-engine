67453e63a83869271d1dbc3e07a48d9936cff931
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2025-02-18 16:55:07 +0100

    feat: update API Conversion for test

--- apps/astro/src/global/constants.ts
+++ apps/astro/src/global/constants.ts
@@ -28,7 +28,7 @@ export const LOCALE: string = 'pl'
  * @constant
  * @type {string}
  */
-export const DOMAIN: string = 'https://localhost:4321'
+export const DOMAIN: string = 'https://landing-page-engine-astro-git-dev-kryptonum.vercel.app'
 
 /**
  * Global declaration of the default title for the application.
